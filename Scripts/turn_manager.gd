49ff89a0a2e0f7a29f9fe5b7ba0bd3662fc69e6c
Isaac Thompson <iamthompson96@gmail.com>
2026-02-05 23:37:39 +0000

    Build out initiative system, factoring in dexterity and resolve

--- Scripts/turn_manager.gd
+++ Scripts/turn_manager.gd
@@ -11,7 +11,15 @@ func start_battle(unit_list: Array) -> void:
 	# Set all units to waiting first
 	for unit in units:
 		unit.change_state("Waiting")
+
+	units.sort_custom(_sort_by_initiative)
+
 	start_turn()
+
+func _sort_by_initiative(a, b):
+	var initiative_a = round((a.data.dexterity * 1.2) + (a.data.resolve * 0.6) + (randi() % 10 * 0.3))
+	var initiative_b = round((b.data.dexterity * 1.2) + (b.data.resolve * 0.6) + (randi() % 10 * 0.3))
+	return initiative_b - initiative_a  # Sort in descending order
 
 func start_turn() -> void:
 	if units.is_empty():
